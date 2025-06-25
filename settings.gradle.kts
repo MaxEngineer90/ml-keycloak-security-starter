1bc2e7c1ac5dc423164183d5f6da56f81719d23f
lammmax <maximilianlamm.kontakt@outlook.de>
2025-06-25 14:51:07 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "ml-keycloak-security-starter"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/java/io/github/max/engineer/ml/keycloak/security/MlKeycloakSecurityStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.max.engineer.ml.keycloak.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlKeycloakSecurityStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlKeycloakSecurityStarterApplication.class, args);
+	}
+
+}
