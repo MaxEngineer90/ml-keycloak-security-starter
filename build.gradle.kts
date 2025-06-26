2396c371e5f3e29bc89d41d792c66657e7f3960f
lammmax <maximilianlamm.kontakt@outlook.de>
2025-06-26 15:23:56 +0200

    fix(build): correct version definition order for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,12 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+            
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            
+            suppressAllPomMetadataWarnings()
         }
     }
 }
